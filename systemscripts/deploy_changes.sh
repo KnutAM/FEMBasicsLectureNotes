7d143563f2654cf4d92f114b5133a7d024fbe7cf
Knut Andreas <knutam@gmail.com>
2025-10-22 15:21:01 +0200

    Add certificate renewal

--- systemscripts/deploy_changes.sh
+++ systemscripts/deploy_changes.sh
@@ -8,3 +8,15 @@ setsebool -P httpd_can_network_connect 1
 cp -p "$SCRIPT_DIR/plutosliderserver.service" "/etc/systemd/system/plutosliderserver.service"
 
 cp -p "$SCRIPT_DIR/pluto.conf" "/etc/nginx/conf.d/pluto.conf"
+
+cp -p "$SCRIPT_DIR/certbot-renew.service" /etc/systemd/system/certbot-renew.service
+
+cp -p "$SCRIPT_DIR/certbot-renew.timer" /etc/systemd/system/certbot-renew.timer
+
+
+systemctl daemon-reload
+systemctl enable --now plutosliderserver.service
+systemctl enable --now certbot-renew.timer
+
+nginx -t
+systemctl enable --now nginx
