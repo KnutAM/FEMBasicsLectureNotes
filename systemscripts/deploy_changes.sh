7ee1f0d474d6834fb986e278f3b4384f3bebb54a
Knut Andreas <knutam@gmail.com>
2025-10-22 14:23:04 +0200

    Allow network connection

--- systemscripts/deploy_changes.sh
+++ systemscripts/deploy_changes.sh
@@ -2,6 +2,9 @@
 
 SCRIPT_DIR="/home/pluto/sites/FEMBasicsLectureNotes/systemscripts"
 
+# Allow web server to make outbound connections 
+setsebool -P httpd_can_network_connect 1
+
 cp -p "$SCRIPT_DIR/plutosliderserver.service" "/etc/systemd/system/plutosliderserver.service"
 
 cp -p "$SCRIPT_DIR/pluto.conf" "/etc/nginx/conf.d/pluto.conf"
