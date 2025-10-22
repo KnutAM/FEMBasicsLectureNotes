68e8e2c2669859fdb1b887ff7aa629f05b901d3f
Knut Andreas <knutam@gmail.com>
2025-10-22 14:12:56 +0200

    Add deploy of pluto.conf

--- systemscripts/deploy_changes.sh
+++ systemscripts/deploy_changes.sh
@@ -3,3 +3,5 @@
 SCRIPT_DIR="/home/pluto/sites/FEMBasicsLectureNotes/systemscripts"
 
 cp -p "$SCRIPT_DIR/plutosliderserver.service" "/etc/systemd/system/plutosliderserver.service"
+
+cp -p "$SCRIPT_DIR/pluto.conf" "/etc/nginx/conf.d/pluto.conf"
