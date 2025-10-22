fbfb859bb06c194abb14353e5f15f9db18c3c741
Knut Andreas <knutam@gmail.com>
2025-10-22 12:58:12 +0200

    Remove some comments

--- systemscripts/run-pluto-slider.sh
+++ systemscripts/run-pluto-slider.sh
@@ -2,10 +2,8 @@
 set -euo pipefail
 cd "$HOME/sites/FEMBasicsLectureNotes"
 
-# Choose a data dir for exports & logs
 export JULIA_NUM_THREADS=auto
 
-# Note: tweak arguments to match your repo & needs
 exec julia--project="pluto-slider-server-environment" -e '
 import Pkg; Pkg.instantiate(); 
 import PlutoSliderServer; 
