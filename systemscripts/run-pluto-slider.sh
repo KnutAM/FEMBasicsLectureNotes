7510f61882eb194069688b67708cb6087e373a28
Knut Andreas <knutam@gmail.com>
2025-12-03 16:51:15 +0100

    Use plutosliderserver static export

--- systemscripts/run-pluto-slider.sh
+++ systemscripts/run-pluto-slider.sh
@@ -7,5 +7,7 @@ export JULIA_NUM_THREADS=auto
 exec $HOME/.juliaup/bin/julia --project="pluto-slider-server-environment" -e '
 import Pkg; Pkg.instantiate(); 
 import PlutoSliderServer; 
-PlutoSliderServer.run_git_directory(".")
+PlutoSliderServer.run_git_directory("."; 
+    static_export=true,
+    Export_output_dir="static")
 '
